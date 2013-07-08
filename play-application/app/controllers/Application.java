5bbf8d9a3903ee618b726197184bc45c404bd79a
Glyn Normington <gnormington@gopivotal.com>
2013-07-08 10:52:39 +0100

    Configure database and display database configuration

--- play-application/app/controllers/Application.java
+++ play-application/app/controllers/Application.java
@@ -9,10 +9,27 @@ import views.html.*;
 import util.Probe;
 
 public class Application extends Controller {
+
+	private static final String DRIVER_KEY = "db.default.driver";
+
+	private static final String URL_KEY = "db.default.url";
+
+	private static final String USER_KEY = "db.default.user";
+
+	private static final String PASSWORD_KEY = "db.default.password";
  
     public static Result index() {
     	Probe probe = Spring.getBeanOfType(Probe.class);
-        return ok(index.render("Input arguments: " + probe.getInputArguments()));
+    	String driverValue = Play.application().configuration().getString("DRIVER_KEY");
+        String urlValue = Play.application().configuration().getString("URL_KEY");
+        String userValue = Play.application().configuration().getString("USER_KEY");
+        String passwordValue = Play.application().configuration().getString("PASSWORD_KEY");
+    	
+        return ok(index.render("Input arguments: " + probe.getInputArguments() + "\n" +
+            "Database configuration:\n" + DRIVER_KEY + "=" + driverValue + "\n" +
+            URL_KEY + "=" + urlValue + "\n" +
+            USER_KEY + "=" + userValue + "\n" +
+            PASSWORD_KEY + "=" + passwordValue));
     }
   
 }
